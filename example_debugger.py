d4d5be1b1bda461d6789a27090d278c22e058cd9
Ivan Covach <eeiprex@gmail.com>
2023-05-18 17:04:39 +0300

    [add] Custom debugger on python

---
+++ example_debugger.py
@@ -0,0 +1,17 @@
+﻿import debugger as mydbg
+
+
+def foo(a, b):
+    for i in range(3):
+        a += i
+        print(a)
+
+    return a + b
+
+
+if __name__ == '__main__':
+
+    mydbg.set_trace()
+    result = foo(1, 2)
+    a = result + 3
+    print(result)
